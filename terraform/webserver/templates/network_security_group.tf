630b376ed04b2270fffbb95828e52f4b475b7fc9
norshtein <tosi@microsoft.com>
2018-11-01 12:14:41 +0800

    Add new nsg rule

--- terraform/webserver/templates/network_security_group.tf
+++ terraform/webserver/templates/network_security_group.tf
@@ -4,7 +4,7 @@ resource "azurerm_network_security_group" "jindou_network_security_group" {
   resource_group_name = "${azurerm_resource_group.jindou_resource_group.name}"
 }
 
-resource "azurerm_network_security_rule" "jindou_network_security_rule" {
+resource "azurerm_network_security_rule" "jindou_network_security_rule_http" {
   name                        = "Allow-Http"
   priority                    = 100
   direction                   = "Inbound"
@@ -12,6 +12,20 @@ resource "azurerm_network_security_rule" "jindou_network_security_rule" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.jindou_resource_group.name}"
+  network_security_group_name = "${azurerm_network_security_group.jindou_network_security_group.name}"
+}
+
+resource "azurerm_network_security_rule" "jindou_network_security_rule_ssh" {
+  name                        = "Allow-Ssh"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "${azurerm_resource_group.jindou_resource_group.name}"
