c32d3d5412b6de6407696cc8ccda22e03e83964f
Bin Xia <binxi@microsoft.com>
2018-11-14 15:10:36 +0800

    Bump common, artifacts and tfhandler (#22)
    * Bump common, artifacts and tfhandler
    
    * Remove tests

--- vendor/github.com/kun-lun/artifacts/pkg/apis/load_balancer.go
+++ vendor/github.com/kun-lun/artifacts/pkg/apis/load_balancer.go
@@ -29,6 +29,23 @@ type LoadBalancerRule struct {
 	HealthProbeName        string `yaml:"health_probe_name"`
 }
 
+func loadBalancerValidator(m Manifest) error {
+	for _, lb := range m.LoadBalancers {
+		occuredNames := make(map[string]bool)
+		if lb.Name == "" {
+			return validationError("load balancer name can't be empty")
+		}
+		if occuredNames[lb.Name] {
+			return validationError("load balancer name must be unique, but %s occurs at least twice", lb.Name)
+		}
+		occuredNames[lb.Name] = true
+		if lb.SKU != "Basic" && lb.SKU != "Standard" {
+			return validationError("sku for load balancer %s must be \"Standard\" or \"Basic\"", lb.Name)
+		}
+	}
+	return nil
+}
+
 const (
 	LoadBalancerStandardSKU = "standard"
 	LoadBalancerBasicSKU    = "basic"
